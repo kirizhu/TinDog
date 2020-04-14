0bf012e38986c3c833ad9a76e7b66bd5a07b2e57
KiriZhu <kiribaty88@gmail.com>
2020-04-14 16:52:38 +0200

    added media query for title section

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
 function createCheckDigit(membershipId) {
-    let array = membershipId.split('')
-    let sum = 0;
-    array.forEach(number => {
-        sum += parseInt(number)
+  let array = membershipId.split('');
+  let sum = 0;
+  array.forEach(number => {
+    sum += parseInt(number);
+  });
+  if (sum.toString().length > 1) {
+    let second = sum.toString().split('');
+    sum = 0;
+    second.forEach(number => {
+      sum += parseInt(number);
     });
-    if(sum.toString().length > 1){
-        let second = sum.toString().split('')
-        sum = 0
-        second.forEach(number => {
-            sum += parseInt(number)
-        });
-    }
-    return sum;   
   }
-  
-console.log(createCheckDigit("123"));
+  return sum;
+}
+
+console.log(createCheckDigit('123'));
